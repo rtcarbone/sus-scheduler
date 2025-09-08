eea9f5817d6990514545761506f96efffeef8886
Carbone <rtcarbone@gmail.com>
2025-09-08 19:40:53 -0300

    Inicialização do projeto sus-scheduler (Spring Boot + Clean Architecture)

---
+++ src/main/java/br/com/fiap/sus_scheduler/SusSchedulerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sus_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/sus_scheduler/SusSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sus_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
